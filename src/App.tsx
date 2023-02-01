ad6380e0211717eeb59a7805a287b2c190ddbb70
Harsh Codelink <--hsCodelink@gmail.com>
2023-02-01 14:53:40 +0530

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  //String :
+  // const name: string = "harsh";
+
+  //Number array:
+  // const a: number[] = [1, 2, 3, 4, 5];
+  // console.log(a);
+
+  //boolen:
+  // const mixed: any[] = ["Harsh", 10, false];
+  // console.log(mixed);
+
+  //obj:
+  // const user = {
+  //   name: String,
+  //   age: Number,
+  //   gender: Boolean,
+  // };
+
+  // user = {
+  //   name: "Harsh",
+  //   age: 10,
+  //   gender: true,
+  // };
+
+  // enum custome data type:
+
+  // enum dayofTheWeek {
+  //   SUN,
+  //   MON,
+  //   TUE,
+  //   WED,
+  //   THUR,
+  //   FRI,
+  //   SAT,
+  // }
+  // const day: dayofTheWeek = dayofTheWeek.SAT;
+  // console.log("day:", day);
+
+  // if (day === dayofTheWeek.SAT) {
+  //   console.log("Its Monday....!");
+  // } else {
+  //   console.log("Its not monday.........?");
+  // }
+
+  // Tuple Data Type:
+
+  const details: {
+    id: number;
+    name: string;
+    role: [number, string];
+  } = {
+    id: 1,
+    name: "Harsh",
+    role: [1, "Harsh"],
+  };
+
+  console.log("Details: ", details);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div>{name}</div> */}
+      <div>Harsh</div>
+    </>
   );
 }
 
